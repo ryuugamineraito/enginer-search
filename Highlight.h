217ff47487b666ce4f1cde5a5dc19935c50b925f
ryuugamineraito <39651481+ryuugamineraito@users.noreply.github.com>
2018-08-08 20:45:45 +0700

    Update Highlight.h

--- Highlight.h
+++ Highlight.h
@@ -9,15 +9,14 @@ using namespace std;
 #include <string>
 #include <Windows.h>
 #include <cstring>
+#include <vector>
 
-int Nword(string text);						//return the number of individual word inside text
-string* query_arr(string text);				//make an array out of individual word from text
+					
 void color_print(string &text);				//print the word in color
 void normalize(string &text);				//lower case the word + remove punctuation
 void to_lower(string &text);			
 void remove_puctuation(string &word);		
-bool is_qry(string* queries, int n, string word);							//check if a word is == query							
-void highLightQueries(string *queries, int n, string text);					//highlight queries in the text
-void hightlight_para(string file_name, int para, string queries);			//highlight the all the queries in a paragraph (para th) from file_name.txt
+bool is_qry(vector<string> queries, int n, string word);							//check if a word is == query							
+void highLightQueries(vector<string> queries, int n, string text);					//highlight queries in the text
+void hightlight_para(string file_name, int para, vector<string> queries);			//highlight the all the queries in a paragraph (para th) from file_name.txt
 #endif // !HIGHLIGHT
-
